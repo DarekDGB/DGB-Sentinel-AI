a8d1bbb5f666728e30075785871292163669185d
DarekDGB <majescyk@gmail.com>
2026-01-07 21:55:16 +0000

    contract: pin v3 context hashing to explicit sha256 entrypoint

--- src/sentinel_ai_v2/contracts/__init__.py
+++ src/sentinel_ai_v2/contracts/__init__.py
@@ -1,11 +1,12 @@
-from .v3_types import SentinelV3Request, SentinelV3Response, SentinelV3Constraints
+from .v3_hash import HASH_ALGO_V3, canonical_sha256, canonical_hash_v3
 from .v3_reason_codes import ReasonCode
-from .v3_hash import canonical_sha256
+from .v3_types import SentinelV3Request, SentinelV3Response
 
 __all__ = [
+    "HASH_ALGO_V3",
+    "canonical_sha256",
+    "canonical_hash_v3",
+    "ReasonCode",
     "SentinelV3Request",
     "SentinelV3Response",
-    "SentinelV3Constraints",
-    "ReasonCode",
-    "canonical_sha256",
 ]
